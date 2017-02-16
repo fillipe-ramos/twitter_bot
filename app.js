081faf1368544b3b2da865d0ace1203db16320fb
Fillipe <pinho_rox@hotmail.com>
2017-02-16 20:20:25 -0200

    Tweet, retweet

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var TwitterPackage = require('twitter');
+var wordListPath = require('word-list');
+var fs = require('fs');
+var wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
 
 
 var secret = require("./private/secret");
+var user = require("./private/user");
 var Twitter = new TwitterPackage(secret);
 
 var tweets = require("./tweets");
@@ -10,69 +14,64 @@ var maxTweets = tweets.length;
 
 var oldIndex = -1;
 
-var minutes = 5, the_interval = minutes * 60 * 1000;
-/*setInterval(function() {
-  console.log("I am doing my 5 minutes check");
-  Twitter.post('statuses/update', {status: getRandomTweet() + " #PinhoBot"},  function(error, tweet, response){
-	  if(error){
-		console.log(error);
-	  }
+var minutes = 5, the_interval = minutes * 1000//* 60 * 1000;
+
+// setInterval(function() {
+//   console.log("I am doing my 5 minutes check");
+//   tweet();
+//   //checkTweetsToRetweets();
+// }, the_interval);
+
+
+function tweet(){
+	Twitter.post('statuses/update', {status: getRandomTweet() + " #PinhoBot"},  function(error, tweet, response){
+		if(error){
+			console.log(error);
+		}
 	});
-}, the_interval);*/
+}
 
-Twitter.get('statuses/user_timeline', {screen_name: '@leninho', count: 2},  function(error, data){
-	  if(error){
-		console.log(error);
-	  }else{
-		  for(var i=0; i < data.length; i++){
-			  if(data[i].retweeted_status == null){
-				  console.log("Nao eh retweet");
-				  if(data[i].retweeted == false){
-					console.log("Nao foi retwittado");
-					// grab ID of tweet to retweet
-					var retweetId = data[i].id_str;
-					console.log(retweetId);
-					// Tell TWITTER to retweet
-					/*Twitter.post('statuses/retweet/:id', {
-						id: retweetId
-					}, function(err, response) {
-						if (response) {
-							console.log('Retweeted!!!');
-						}
-						// if there was an error while tweeting
-						if (err) {
-							console.log('Something went wrong while RETWEETING... Duplication maybe...');
-						}
-					});*/
-				  }else{
-					  console.log("Ja foi retwittado");
-				  }
-			  }else{
+
+function checkTweetsToRetweets(){
+	Twitter.get('statuses/user_timeline', {screen_name: user.user_name},  function(error, data){
+		if(error){
+			console.log(error);
+		}else{
+			for(var i=0; i < data.length; i++){
+				if(data[i].retweeted_status == null){
+					if(data[i].retweeted == false){
+						console.log("Nao foi retwittado");
+						// grab ID of tweet to retweet
+						var retweetId = data[i].id_str;
+						// Tell TWITTER to retweet
+						retweet(retweetId);
+					}else{
+						console.log("Ja foi retwittado");
+					}
+				}else{
 					console.log("Eh retweet");
-			  }
-		  }
-		  //console.log(data);
-			/*/ grab ID of tweet to retweet
-            var retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted!!!');
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
-                }
-            });*/
-	  }
-});
+				}
+			}
+		}
+	});
+}
 
+function retweet(retweetId){
+	Twitter.post('statuses/retweet/' + retweetId , function(err, response) {
+		if (response) {
+			console.log('Retweeted!!!');
+			console.log(response);
+		}
+		// if there was an error while tweeting
+		if (err) {
+			console.log('Something went wrong while RETWEETING... Duplication maybe...');
+		}
+	});
+}
 
 function getRandomTweet() {
-    //calculate a random index
-    var index = Math.floor(Math.random() * (maxTweets - 1));
+	//calculate a random index
+	var index = Math.floor(Math.random() * (maxTweets - 1));
 	if(oldIndex == index){
 		if(index == 0){
 			index+1;
@@ -81,9 +80,9 @@ function getRandomTweet() {
 			index-1;
 		}
 	}
-    //return the random sentence
+	//return the random sentence
 	oldIndex = index;
-    return tweets[index];
+	return tweets[index];
 }
 
 
